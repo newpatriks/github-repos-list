8d0b41cbbb3f3ce282d73d5572d216595102b605
Jordi Llobet <jordi.llobet@me.com>
2014-01-21 19:21:37 +0100

    Deleted requirejs plugin and all his dependences.
    - Deleted requirejs plugin
    - Deleted !text plugin to load templates
    - Templates now are loading with underscore
    - All templates are in html code inside index.html
    - All files have been cleaned of code

--- js/app/router.js
+++ js/app/router.js
@@ -1,62 +1,36 @@
-define(function (require) {
-    "use strict";
-
-    var $           = require('jquery'),
-        Backbone    = require('backbone.min'),
-        Modernizr 	= require('modernizr.min');
-        
-        
-        
-    var AppRouter = Backbone.Router.extend({
-        routes	: {
-        	""				: 	"home",
-        	"/"				: 	"home",
-            "/repos"		:	"home",
-            "/repo/:id"		:	"details"
-        },
-                
-		initialize 	: 	function() {
-			// Loading Header View			
-		},
-		
-		initView 	:	function(view) {
-			if (this.currentView) {
-				//this.currentView.remove();
-			}
-			this.currentView = view;
-		},
-		
-		home		:	function () {
-			var that = this;
-    		require(["app/views/Header"], function(Header) {
-				var header = new Header({el : "header"});
-				header.render();
-			});
+var AppRouter = Backbone.Router.extend({
+    routes	: {
+    	""				: 	"home",
+        "repo/:id"		:	"details"
+    },
+            
+	initialize 	: 	function() {
+		// Loading Header View
+		this.currentView = null;
+		var header = new viewHeader({el : "header"});
+		header.render();
+	},
+	
+	initView 	:	function(view) {
+		console.log("initView");
+		if (this.currentView) {
 			
-    		require(["app/views/Home"], function(Home) {
-				var home = new Home({el : "#main_content"});
-				home.render();
-				that.initView(home);
-			});
-        },
-        
-        details		: 	function(id) {
-	        var that = this;
-    		require(["app/views/Header"], function(Header) {
-				var header = new Header({el : "header"});
-				header.render();
-			});
-			
-    		require(["app/views/Details"], function(Details) {
-				var details = new Details({el : "#main_content"});
-				details.render(id);
-				that.initView(details);
-			});
-        }
-    });
-    
-    return {
-	    AppRouter : AppRouter
-    };
-    
+		}
+		this.currentView = view;
+		this.currentView.render();
+	},
+	
+	home		:	function () {
+		console.log("home");
+		this.home = new viewHome({el : "#main_content"});
+		this.home.render();
+    },
+    details		: 	function(id) {
+	    console.log("details");
+		this.details = new viewDetails({el : "#main_content", id : id});
+		this.details.render();
+    }
 });
+
+var rout = new AppRouter();
+Backbone.history.start();
