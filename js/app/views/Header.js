ac9c014db466f269242254e72f847136669a6aa2
Jordi Llobet <jordi.llobet@me.com>
2014-01-21 19:21:37 +0100

    Deleted requirejs plugin and all his dependences.
    - Deleted requirejs plugin
    - Deleted !text plugin to load templates
    - Templates now are loading with underscore
    - All templates are in html code inside index.html
    - All files have been cleaned of code

--- js/app/views/Header.js
+++ js/app/views/Header.js
@@ -1,33 +1,20 @@
-define(function (require) {
-
-    "use strict";
-
-    var $			= require('jquery'),
-        _           = require('underscore.min'),
-        Backbone    = require('backbone.min'),
-        tpl         = require('text!tpl/Header.html'),
-        model		= require('app/models/model'),
-        template 	= _.template(tpl);
-
-    return Backbone.View.extend({
-    	el 				: 	$("header"),
-		initialization	:	function() {
-			console.log("[Home]");
-	    },
-        render			: 	function () {
-            console.log("[Home Render]");
-            var that = this;
-            var user = new model.m_UserDetails();
-            user.fetch({
-            	add : false,
-            	success: function(model, response) {            		
-            		model.set("login",response['name'])
-            		model.set('avatar', response['avatar_url']);
-            	}
-            }).complete(function(resp_repo) {
-            	that.$el.html(template({profile : user}));
-            });
-            return this;           	
-        }
-	});
+var viewHeader = Backbone.View.extend({
+	el 				: 	$("header"),
+	initialization	:	function() {
+    },
+    render			: 	function () {
+        var that = this;
+        var user = new m_UserDetails();
+        user.fetch({
+        	add : false,
+        	success: function(model, response) {            		
+        		model.set("login",response['name'])
+        		model.set('avatar', response['avatar_url']);
+        	}
+        }).complete(function(resp_repo) {
+        	var template = _.template( $("#tpl_header").html(), {profile : user} );
+        	that.$el.html(template);
+        });
+        return this;           	
+    }
 });
